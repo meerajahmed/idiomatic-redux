29a6ac6083dfadb9c84b97b85de901aefc9bd036
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2020-05-13 20:37:02 +0530

    use components own props inside mapStateToProps and mapDispatchToProps

--- src/containers/molecules/FilterLink/index.js
+++ src/containers/molecules/FilterLink/index.js
@@ -1,50 +1,25 @@
-import React, { Component } from 'react';
 import FilterButton from '../../../components/atoms/FilterButton';
-import { ReactReduxContext } from '../../../lib/react-redux/Context';
+import { connect } from '../../../lib/react-redux';
 
 /**
  * As a container component, the filter link doesn't have its own markup.
- * It delegates rendering to the link presentational component
+ * It delegates rendering to the FilterButton (Link) presentational component
  * */
-class FilterLink extends Component {
-  constructor(props) {
-    super(props);
-    this.unsubscribe = null;
-  }
 
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate();
-    });
-  }
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter,
+  };
+};
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () =>
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter,
+      }),
+  };
+};
 
-  render() {
-    // eslint-disable-next-line react/prop-types
-    const { filter, children } = this.props;
-    const { store } = this.context;
-    // eslint-disable-next-line react/prop-types
-    const state = store.getState();
-    return (
-      <FilterButton
-        active={filter === state.visibilityFilter}
-        onClick={() => {
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter,
-          });
-        }}
-      >
-        {children}
-      </FilterButton>
-    );
-  }
-}
-
-FilterLink.contextType = ReactReduxContext;
-
-export default FilterLink;
+export default connect(mapStateToProps, mapDispatchToProps)(FilterButton);

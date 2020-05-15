5efd30e78da162e102d0097e7d3535517eed583c
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2020-05-15 15:41:16 +0530

    Middleware: put custome behaviour before action reaches the reducers

--- src/containers/organisms/Todos/configureStore.js
+++ src/containers/organisms/Todos/configureStore.js
@@ -1,35 +1,44 @@
 import createStore from '../../../lib/redux/createStore';
 import reducers from './reducers';
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    /*
-     * Now, we can dispatch both actions and promises that resolve to actions
-     * */
-    if (typeof action.then === 'function') {
-      /* Wait for the promise to resolve before dispatching the action */
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const promise = () => (next) => (action) => {
+  /*
+   * Now, we can dispatch both actions and promises that resolve to actions
+   * */
+  if (typeof action.then === 'function') {
+    /* Wait for the promise to resolve before dispatching the action */
+    return action.then(next);
+  }
+  return next(action);
 };
-
-const addLoggingToDispatch = (store) => {
+/*
+ * mental model: function with several arguments that are applied as they become available
+ * */
+const logging = (store) => (next) => {
   /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color:grey', store.getState());
     console.log('%c action', 'color:blue', action);
-    rawDispatch(action);
+    next(action);
     console.log('%c next state', 'color:green', store.getState());
     console.groupEnd();
   };
   /* eslint-disable no-console */
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  // slice -> clone
+  middlewares
+    .slice()
+    .reverse()
+    .forEach(() => {
+      // eslint-disable-next-line no-param-reassign
+      store.dispatch = middlewares(store)(store.dispatch);
+    });
 };
 
 const configureStore = () => {
@@ -38,18 +47,22 @@ const configureStore = () => {
 
   /**
    * the order in which we override the dispatch function is important
-   * logging <-- promise
+   * logging <-- promise (order in which action propagate)
    * promises should be resolved before the action is logged
    * */
 
   /* const store = createStore(reducers, preloadedState); */
   const store = createStore(reducers);
+  const middlewares = [promise]; // order in which the action propagate
 
   if (__DEV__) {
-    store.dispatch = addLoggingToDispatch(store);
+    // store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logging);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  // store.dispatch = addPromiseSupportToDispatch(store); order in which the dispatch is overridden
+
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   /*
   store.subscribe(
